46bf11bded308183513c59c42cfa5415a803392a
181220067-ZhangHanxiao <181220067@smail.nju.edu.cn>
2020-07-03 23:30:03 +0800

    grid search of xgboost

--- algorithm/v0_xgboost_alg.py
+++ algorithm/v0_xgboost_alg.py
@@ -4,6 +4,7 @@ Created on Fri Jun  5 20:20:19 2020
 
 @author: zhanghanxiao
 """
+
 #---------------------------dependencies-----------------------------
 import pandas as pd
 import numpy as np
@@ -12,14 +13,13 @@ from sklearn.model_selection import learning_curve
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
-# from sklearn.linear_model import LinearRegression
-# from sklearn.tree import DecisionTreeRegressor
-# from xgboost import XGBRegressor
+from sklearn.model_selection import GridSearchCV
 import matplotlib.pyplot as plt
 import time
 from copy import deepcopy
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
+
 #---------------------------const value -----------------------------
 road_name = ['NanPing_W2E', 'NanPing_E2W', 'FuLong_S2N', 'FuLong_N2S', 'LiuXian_W2E', 'LiuXian_E2W',
              'YuLong_S2N', 'YuLong_N2S', 'XinQu_S2N', 'XinQu_N2S', 'ZhiYuan_S2N', 'ZhiYuan_N2S']
@@ -56,69 +56,80 @@ def gen_train(train_data):
     return feature, label
 
 def train(train_X, train_y, eval_X, eval_y,road_index):
-    # params = {
-    #     'booster': 'gbtree',
-    #     'objective': 'reg:gamma',
-    #     'gamma': 0.1,
-    #     'max_depth': 6,
-    #     'lambda': 3,
-    #     'subsample': 0.7,
-    #     'colsample_bytree': 0.7,
-    #     'min_child_weight': 3,
-    #     'eta': 0.1,
-    #     'seed': 1000,
-    #     'nthread': 4,
-    # }
-    # num_round = 1
-    # # plst = params.items()
-    # evallist = [(deval, 'eval'), (dtrain, 'train')]
-    # bst = xgb.train(params, dtrain, num_round, evallist)
-    # model_name = road_name[road_index] + ".model"
-    # bst.save_model(model_name)
-    # return bst
-    #model_total = []
+    dict_para = {'n_estim': [[50,50,120], [140,50,80], [60,140,80], [50,50,150], [50,140,140], [100,50,100],
+                             [50,150,70], [120,50,150], [150,50,80], [50,70,80], [60,50,150], [50,50,80]],
+                 'max_de': [[5,6,4], [6,4,6], [9,5,6], [3,4,3], [6,4,4], [2,3,7],
+                            [5,6,4], [6,6,4], [5,6,4], [5,5,8], [5,6,4], [6,6,6]],
+                 'min_child_wei': [[2,4,3], [1,4,1], [5,7,10], [1,9,7], [2,4,7], [2,9,9],
+                                   [9,10,6], [3,8,8], [5,6,7], [6,7,8], [5,8,7], [7,8,9]],
+                 'para_gamma': [[0.13,0.19,0.08], [0.0,0.04,0.0], [0.05,0.08,0.05], [0.04,0.01,0.05], [0.03,0.05,0.1], [0.09,0.04,0.06],
+                                [0.01,0.03,0.05], [0.06,0.08,0.06], [0.01,0.02,0.0], [0.01,0.02,0.01], [0.03,0.1,0.0], [0.02,0.05,0.08]],
+                 'subsample': [[0.6,1.0,1.0], [0.9,0.9,1.0], [0.8,0.8,0.9], [0.7,1.0,0.7], [0.6,1.0,0.9], [0.8,0.8,0.9],
+                               [0.6,1.0,1.0], [0.6,0.7,0.8], [1.0,1.0,0.9], [0.7,0.8,1.0], [0.7,0.6,0.7], [0.7,0.7,0.9]],
+                 'colsample_bytree': [[0.8,1.0,1.0], [1.0,1.0,1.0], [0.8,0.8,1.0], [0.8,0.9,1.0], [0.9,1.0,1.0], [0.4,1.0,0.8],
+                                      [0.9,0.7,1.0], [1.0,1.0,1.0], [0.5,1.0,1.0], [0.8,1.0,1.0], [0.9,0.9,1.0], [0.8,1.0,0.7]],
+                 'reg_lambda': [[10,30,50], [0,30,0], [10,30,50], [10,10,0], [10,0,30], [10,40,50],
+                                [10,20,40], [50,50,50], [0,20,0], [20,60,50], [10,30,60], [30,60,30]],
+                 'reg_alpha': [[0.7,0.0,0.6], [0.0,0.9,0.3], [0.7,0.2,0.4], [0.7,0.4,0.4], [0.7,0.3,0.0], [0.9,0.1,0.1],
+                               [0.6,0.1,0.0], [0.2,0.7,1.0], [1.0,0.0,0.7], [0.5,0.6,0.0], [0.7,0.8,0.7], [0.7,0.9,0.5]],
+                 'eta': [[0.2,0.3,0.1], [0.1,0.2,0.1], [0.1,0.1,0.1], [0.2,0.2,0.02], [0.1,0.07,0.1], [0.2,0.3,0.1],
+                         [0.1,0.3,0.1], [0.2,0.2,0.07], [0.3,0.2,0.1], [0.2,0.2,0.2], [0.1,0.3,0.1], [0.2,0.2,0.07]]};
     train_y = train_y.T
     eval_y = eval_y.T
     model = [0,0,0]
+    gs = [0,0,0]
+    best_params = []
+    best_score = []
     #print(train_y.shape)
     for i in range(3):
-        model[i] = xgb.XGBRegressor(max_depth=6, learning_rate=0.1, n_estimators=160, min_child_weight=1,
-                                    subsample=0.8, colsample_bytree=0.8, gamma=0,
-                                    reg_alpha=0, reg_lambda=1)
-        if i==0:
-            model[i].fit(train_X, train_y[i],
-                    eval_set = [(eval_X, eval_y[i])],
-                    eval_metric='mae',
-                    early_stopping_rounds = 10)
-        elif i==1 or i==2:
+        other_params = {'eta': dict_para['eta'][road_index][i], 'n_estimators': dict_para['n_estim'][road_index][i]+400,
+                        'gamma': dict_para['para_gamma'][road_index][i], 'max_depth': dict_para['max_de'][road_index][i],
+                        'min_child_weight': dict_para['min_child_wei'][road_index][i], 'colsample_bytree': dict_para['colsample_bytree'][road_index][i],
+                        'colsample_bylevel': 1, 'subsample': dict_para['subsample'][road_index][i], 'reg_lambda': dict_para['reg_lambda'][road_index][i],
+                        'reg_alpha': dict_para['reg_alpha'][road_index][i], 'seed': 33}
+        
+        # cv_params = {'n_estimators': np.linspace(50, 150, 11, dtype=int)}
+        # cv_params = {'max_depth': np.linspace(1, 10, 10, dtype=int)}
+        # cv_params = {'min_child_weight': np.linspace(1, 10, 10, dtype=int)}
+        # cv_params = {'gamma': np.linspace(0, 0.1, 11)}
+        # cv_params = {'subsample': np.linspace(0, 1, 11)}
+        # cv_params = {'colsample_bytree': np.linspace(0, 1, 11)[1:]}
+        # cv_params = {'reg_lambda': np.linspace(0, 100, 11)}
+        # cv_params = {'reg_alpha': np.linspace(0, 1, 11)}
+        # cv_params = {'eta': np.logspace(-2, 0, 10)}
+
+        model[i] = xgb.XGBRegressor(**other_params)
+        
+        if i >= 1:
             pre1 = model[i-1].predict(train_X)
             tmp = []
             for j in range(pre1.shape[0]):
                 tmp.append([pre1[j]])
             tmp = np.array(tmp)
             train_X = np.hstack((train_X,tmp))
-            
+
             pre1 = model[i-1].predict(eval_X)
             tmp = []
             for j in range(pre1.shape[0]):
                 tmp.append([pre1[j]])
             tmp = np.array(tmp)
-            eval_X = np.hstack((eval_X,tmp))
-            
-            
-            model[i].fit(train_X, train_y[i],
-                    eval_set = [(eval_X, eval_y[i])],
-                    eval_metric='mae',
-                    early_stopping_rounds = 10)
+            eval_X = np.hstack((eval_X,tmp))  # has eight columns
         
-            
-            
+        # gs[i] = GridSearchCV(model[i], cv_params, verbose=2, refit=True, cv=5, n_jobs=-1)
+        # gs[i].fit(train_X, train_y[i])
+        # best_params.append(gs[i].best_params_)
+        # best_score.append(gs[i].best_score_)
+    # print("参数的最佳取值：:", best_params)
+    # print("最佳模型得分:", best_score)   
+    # models.append(gs)
+        
+        model[i].fit(train_X, train_y[i],
+                eval_set = [(eval_X, eval_y[i])],
+                eval_metric='mae',
+                early_stopping_rounds = 50)
+
     models.append(model)
-    #model_total.append(model)
-    #models.append(model_total)
     return model
-        
-    
 
 def gen_test(test_data):
     feature = []
@@ -132,9 +143,6 @@ def gen_test(test_data):
         feature.append(tmp)
     feature = np.array(feature)
     return feature
-    
-    
-    
     
 '''   
     pred_df['pred1'] = None
@@ -181,7 +189,6 @@ def predict(model,X_test):
     lst3 = []
     lst1 = model[0].predict(X_test)
     
-    
     pre1 = lst1
     tmp = []
     for j in range(pre1.shape[0]):
@@ -203,25 +210,22 @@ def predict(model,X_test):
     return lst
     
 
-        
-        
-        
-            
-                
-
 def main():
     mae = 0
-    #pre_df_lst = [0,0,0,0,0,0,0,0,0,0,0,0]
     pre_df_lst = [0,0,0,0,0,0,0,0,0,0,0,0]
 
     for i in range(12):
+        # X_filename = 'train_array/X_12_' + road_name[i] + '.npy'
+        # X = np.load(X_filename)
+        # y_filename = 'train_array/y_12_' + road_name[i] + '.npy'
+        # y = np.load(y_filename)
         train_data = pd.read_csv("../datasets/train_0103_"+road_name[i]+".csv", sep=',')
         train_data = train_data.sort_values(by = 'timestamp')
         test_data = pd.read_csv("../datasets/stage2_test_"+road_name[i]+".csv", sep=',')
         test_data = test_data.sort_values(by = 'timestamp')
-        X, y = gen_train(train_data)#np array
-        X_test = gen_test(test_data)
+        X, y = gen_train(train_data)  # np array
         y = y.T
+        X_test = gen_test(test_data)
         #PCA 
         #pca1 = PCA(n_components = 20)
         #pca2 = PCA(n_components = 20)
@@ -238,10 +242,10 @@ def main():
         #print(cluster_label)
         
        # train_X, eval_X, train_y, eval_y,cluster_X,cluster_y = train_test_split(X,y,cluster_label[:X.shape[0]],test_size=0.25,random_state=1591545677)
-        train_X, eval_X, train_y, eval_y = train_test_split(X,y,test_size=0.25,random_state=1591545677)
+        train_X, eval_X, train_y, eval_y = train_test_split(X, y, test_size=0.25, random_state=1591545677)
         #train_y.shape = (...,3)
         
-        model= train(train_X, train_y, eval_X, eval_y,i)
+        model = train(train_X, train_y, eval_X, eval_y,i)
         mae += evaluate(model, eval_X, eval_y)
         #print(X_test)
         pre_df = predict(model,X_test)
@@ -279,7 +283,7 @@ def main():
             assert(0)
     #print(time_cost)
     #print(result)
-    result.to_csv("../model_result/xgb2.csv")
+    result.to_csv("../model_result/xgb4.csv")
     #pd.DataFrame.to_csv(noLabel['pred'], "D:/test_data/pred_TTI3.csv", sep=',')
 
 if __name__ == "__main__":
